98157da9c36a62fe40fc39acfccef3502a02b71a
Kallel Roman <kallelr@outlook.com>
2020-11-03 23:58:27 -0300

    ãdicionando modais e tratamentos

--- public/bandersnatch/src/videoPlayer.js
+++ public/bandersnatch/src/videoPlayer.js
@@ -1,9 +1,12 @@
 class VideoMediaPlayer {
-  constructor ({ manifestJSON, network }) {
+  constructor({ manifestJSON, network, videoComponent }) {
     this.manifestJSON = manifestJSON;
     this.videoElement = null;
     this.videoBuffer = null;
+    this.videoComponent = videoComponent;
     this.network = network;
+
+    this.activeItem = {}
     this.selected = {};
     this.videoDuration = 0;
   }
@@ -28,14 +31,41 @@ class VideoMediaPlayer {
     mediaSource.addEventListener("sourceopen", this.sourceOpenWrapper(mediaSource))
   }
 
-  sourceOpenWrapper(mediaSource){
-    return async(_) => {
+  sourceOpenWrapper(mediaSource) {
+    return async (_) => {
       this.sourceBuffer = mediaSource.addSourceBuffer(this.manifestJSON.codec);
       const selected = this.selected = this.manifestJSON.intro;
 
       mediaSource.duration = this.videoDuration;
       await this.fileDownload(selected.url);
+      setInterval(this.waitForQuestions.bind(this), 200)
     };
+  }
+
+  waitForQuestions() {
+    const currentTime = parseInt(this.videoElement.currentTime);
+    const option = this.selected.at === currentTime;
+    if (!option) return;
+
+    // evita que o modal apareça duas vezes no mesmo segundo
+    if (this.activeItem.url === this.selected.url) return;
+    this.videoComponent.configureModal(this.selected.options)
+    this.activeItem = this.selected;
+  }
+
+  async nextChunk(data) {
+    const key = data.toLowerCase();
+    console.log(key, data)
+    const selected = this.manifestJSON[key];
+    console.log(selected, this.manifestJSON[key])
+    this.selected = {
+      ...selected,
+      // ajusta o tempo que o modal vai aparecer baseado no tempo corrente
+      at: parseInt(this.videoElement.currentTime + selected.at),
+    }
+    // deixa o restante do vídeo rodar enquanto baixa o novo video
+    this.videoElement.play()
+    await this.fileDownload(selected.url)
   }
 
   async fileDownload(url) {
@@ -45,7 +75,9 @@ class VideoMediaPlayer {
       fileResolutionTag: this.manifestJSON.fileResolutionTag,
       hostTag: this.manifestJSON.hostTag,
     }
+    console.log(prepareUrl)
     const finalUrl = this.network.parseManifestURL(prepareUrl);
+    console.log({ finalUrl })
     this.setVideoPlayerDuration(finalUrl)
     const data = await this.network.fetchFile(finalUrl);
     return this.processBufferSegments(data)
@@ -53,8 +85,8 @@ class VideoMediaPlayer {
 
   setVideoPlayerDuration(finalURL) {
     const bars = finalURL.split('/');
-    const [, videoDuration ] = bars[bars.length - 1].split('-');
-    this.videoDuration += videoDuration;
+    const [, videoDuration] = bars[bars.length - 1].split('-');
+    this.videoDuration += parseFloat(videoDuration);
   }
 
   async processBufferSegments(allSegments) {
